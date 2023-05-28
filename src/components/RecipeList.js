3d89bba38e7e745f54cf8cf3f1bed5be26dca8da
Natalie Gillam <ngillam@hotmail.co.uk>
2023-05-29 00:26:28 +0100

    added logic and styling to fetch and display recipe details

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom"
+import {Link} from 'react-router-dom/cjs/react-router-dom.min';
 
 //styles
 import './RecipeList.css'
@@ -10,13 +10,14 @@ export default function RecipeList({recipes}) {
         <div key={recipe.id} className="card">
             <div className="card-heading">
             <h3>{recipe.title}</h3>
-            <Link to={`/recipes/${recipe.id}`}>See details...</Link>
+            <Link className="button" to={`/recipes/${recipe.id}`}>See details...</Link>
             </div>
             <p>Ready in {recipe.cookingTime}</p>
             <h4>Ingredients</h4>
-            <ul>{recipe.ingredients.map(ingredient=>(<li>{ingredient}</li>))}</ul>
+            <ul>{recipe.ingredients.map(ingredient=>(<li key={ingredient}>{ingredient}</li>))}</ul>
             <h4>Method</h4>
-            <div>{recipe.method.substring(0,80)}...</div>
+            <Link className="link-text" to={`/recipes/${recipe.id}`}>See method...</Link>
+            {/* <div>{recipe.method.substring(0,80)}...</div> */}
             
         </div>
     ))}</div>
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,7 +1,30 @@
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { useFetch } from '../../hooks/useFetch'
+
+// styles
 import './Recipe.css'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const { error, isPending, data: recipe } = useFetch(url)
+
   return (
-    <div>Recipe</div>
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+      <h2 className="page-title">{recipe.title}</h2>
+      <p>Preparation time: {recipe.cookingTime}</p>
+      <h4>Ingredients</h4>
+      <ul>{recipe.ingredients.map(ingredient=>(<li key={ingredient}>{ingredient}</li>))}</ul>
+
+            <h4>Method</h4>
+            <div>{recipe.method}...</div>
+
+      </>
+      )}
+    </div>
   )
 }
