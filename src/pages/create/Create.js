503d00dd54b01dae29343f2931094a5b17d23b40
Natalie Gillam <ngillam@hotmail.co.uk>
2023-05-29 13:07:36 +0100

    added functionality to add ingredients to create recipe form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 
 //styles
 import './Create.css'
@@ -7,11 +7,25 @@ export default function Create() {
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [cookingTime, setCookingTime] = useState('')
-
+  const [newIngredient, setNewIngredient] = useState('')
+  const [ingredients, setIngredients] = useState([])
+  const ingredientInput = useRef(null)
+  
    const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(title, method, cookingTime)
+    console.log(title, method, cookingTime, ingredients)
    }
+
+   const handleAdd = (e) => {
+    e.preventDefault()
+    const ing = newIngredient.trim()
+    if(ing && !ingredients.includes(ing)){
+      setIngredients(prevIngredients => 
+        [...prevIngredients, ing])
+    }
+    setNewIngredient('')
+    ingredientInput.current.focus()
+  }
 
   return (
     <div className="create">
@@ -27,8 +41,22 @@ export default function Create() {
           required/>
         </label>
 
-        {/* ingredients go here */}
-
+        <label htmlFor="recipe-ingredients"><span>Recipe ingredients:</span>
+          <div className="ingredients">
+          <input 
+          type="text" 
+          id="recipe-ingredients"
+          onChange={(e)=>setNewIngredient(e.target.value)}
+          value={newIngredient}
+          ref={ingredientInput}
+          />
+          <button onClick={handleAdd} className="btn">Add</button>
+          </div>
+        </label> 
+        <p>Current ingredients: {ingredients.map((i)=>(
+          <em key={i}>{i}, </em>
+        ))}</p>
+<div>
         <label htmlFor="recipe-method"><span>Recipe method:</span>
           <textarea id="recipe-method"
           onChange={(e)=>setMethod(e.target.value)}
@@ -46,7 +74,7 @@ export default function Create() {
         </label>
 
         <button className="btn">Submit</button>
-
+        </div>
       </form>
     </div>
   )
