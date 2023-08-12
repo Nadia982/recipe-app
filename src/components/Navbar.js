392fe65cb22273b4d7f76c037ab8025143714ff8
Natalie Gillam <ngillam@hotmail.co.uk>
2023-08-13 00:14:45 +0100

    added useReducer to enable changing the navbar colour

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,11 @@ import SearchBar from './SearchBar';
 
 
 export default function Navbar() {
-  const {color} = useTheme();
+  const {color, changeColor} = useTheme();
 
   return (
     <div className="navbar" style={{background: color}}>
-        <nav>
+        <nav onClick={()=> changeColor('hotpink')}>
             <Link to='/' className='brand'>
             <h1>Halal gluten-free recipes</h1>
             </Link>
